54a228d0068c4dfc5d002d5ca160acee50003490
LiadJ9 <127025326+LiadJ9@users.noreply.github.com>
2023-09-13 19:11:09 +0300

    changed url

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Personal-Website/" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Homepage />} />
           <Route path="Blog" element={<Blog />} />
           <Route path="About" element={<AboutMe />} />
